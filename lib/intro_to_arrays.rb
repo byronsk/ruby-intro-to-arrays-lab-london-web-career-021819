651d2ae0edbe44a89caa99adc4a403fe63f343e9
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-04 11:23:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,6 +1,6 @@
 
 def instantiate_new_array
- @my_new_array = []
+  @my_new_array = []
 end
 
 def array_with_two_elements
@@ -19,11 +19,11 @@ def last_element(array)
   array[-1]
 end
 
-def first_element_with_array(array)
+def first_element_with_array_methods(array)
   array.first
 end
 
-def last_element_with_array(array)
+def last_element_with_array_methods(array)
   array.last
 end
 
